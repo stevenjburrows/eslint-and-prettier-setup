5429cba3af3586bff236c3cc0f0f20b1635cb7b7
Steven Burrows <60275700+stevenjburrows@users.noreply.github.com>
2023-12-03 12:08:27 +0000

    added the eslint config with some errors

---
+++ src/app/components/testing.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function testing() {
+
+  // this file has indent and a trailing comma error
+    const array = [
+        1,
+        2,
+        3
+        ]
+
+        console.log('testing');
+
+  return (
+    <div>testing</div>
+  )
+}
